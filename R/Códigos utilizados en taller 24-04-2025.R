6a768cf056f048d8c9c72845d8444c110a06d28c
Juan Cisternas <72153826+jcisternasr@users.noreply.github.com>
2025-04-24 11:49:07 -0400

    agregar código R de limpieza y algunos codigos utilizados en el taller del 24-04-2025

---
+++ R/Códigos utilizados en taller 24-04-2025.R
@@ -0,0 +1,4 @@
+# renv::init()
+rcompendium::add_renv() #alternativa a iniciar renv pero desde rcompendium
+renv::status() #Reports issues caused by inconsistencies across the project lockfile, library, and dependencies()
+renv::install() #instala librerias que faltan de acuerdo a lo señalado previamente con renv::status
